9249f6e512fcd21258f1cd55f4328f2894d01b19
sachinda <sachinda9696@gmail.com>
2021-12-29 22:48:57 +0530

    initialize project

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends React.Component {
+    render() { 
+        return <div>
+            <span></span>
+            <button>Incriment</button>
+            
+        </div>;
+    }
+}
+ 
+export default Counter;  
